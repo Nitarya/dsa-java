a8d087ef7740ef9af31f48aab726937294c47fe8
Nitya <niittss27@gmail.com>
2025-02-17 01:03:16 +0530

    Error correction

--- LeearnYard/Lecture 33/matrix.java
+++ LeearnYard/Lecture 33/matrix.java
@@ -1,49 +1,44 @@
 import java.util.Scanner;
 
-class matrix {
+class MatrixMultiplication {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int C = sc.nextInt();
+        int A = sc.nextInt(); // Rows in Matrix 1
+        int B = sc.nextInt(); // Columns in Matrix 1 / Rows in Matrix 2
+        int C = sc.nextInt(); // Columns in Matrix 2
 
-        int m1[][] = new int[A] [B];
-        int m2[][] = new int[B] [C];
-
-        //A*B with B8C  ==> A*C
+        int m1[][] = new int[A][B];
+        int m2[][] = new int[B][C];
         int ans[][] = new int[A][C];
 
-        //take input for m1
-        for(int i  0; i < A; i++) 
-            for(int j = 0; j < B; j++)
+        // Input for first matrix
+        for (int i = 0; i < A; i++) 
+            for (int j = 0; j < B; j++)
                 m1[i][j] = sc.nextInt();
 
-                //take input for m2
-                for(int i = 0; i < B; i++)
-                    for(int j = 0; j < C; j++)
-                        m2[i][j] = sc.nextInt();
+        // Input for second matrix
+        for (int i = 0; i < B; i++)
+            for (int j = 0; j < C; j++)
+                m2[i][j] = sc.nextInt();
 
-            
-            //Find the elements of the ans.
-            for(int i = 0; i < A; i++)
-                for(int j = 0; j < C; j++) {
+        // Matrix Multiplication
+        for (int i = 0; i < A; i++)
+            for (int j = 0; j < C; j++) {
+                int cur = 0;
+                for (int k = 0; k < B; k++)
+                    cur += m1[i][k] * m2[k][j];
 
-                    //Calculate ans[i][j]
-                    int cur = 0;
-                    for(int k = 0; k < B; k++)
-                        cur += m1[i][k]+m2[k][j];
+                ans[i][j] = cur;
+            }
 
-                        ans[i][j] = cur;
-                }
+        // Print the result matrix
+        for (int i = 0; i < A; i++) {
+            for (int j = 0; j < C; j++)
+                System.out.print(ans[i][j] + " ");
+            System.out.println();
+        }
 
-                //Print the answer matrix
-                for(int i = 0; i < A; i++) {
-                    for(int j = 0; j < C; j++)
-                        System.out.print(ans[i][j] + " ")
-                    System.out.println();
-                }
-
-                sc.close();
+        sc.close();
     }
 }
