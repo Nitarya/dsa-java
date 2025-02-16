2fad171ffc2116ee7537ccde8f455a3c2476d764
Nitya <niittss27@gmail.com>
2025-02-17 00:44:06 +0530

    matrix of a number

---
+++ LeearnYard/Lecture 33/main_second_diagonal.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class main_second_diagonal {
+    public static void main(String[] args) {
+
+// your code goes here
+Scanner sc = new Scanner(System.in);
+
+int N = sc.nextInt();
+
+int arr[] [] = new int[N] [N];
+
+//Take input
+for(int i = 0; i < N; i++) 
+    for(int j = 0; j < N; j++)
+        arr[i] [j] = sc.nextInt();
+
+        //Print the main diagonal
+        for(int i = 0; i < N; i++)
+            System.out.print(arr[i][i] + " ");
+        System.out.println();
+
+        //Print secondary diagonal
+        for(int i = 0, j = N - 1; i < N; i++, j--) {
+            System.out.print(arr[i] [j] + " ");
+        }
+        System.out.println();
+    }
+}
